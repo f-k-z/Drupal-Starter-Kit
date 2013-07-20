799a16560fe3b2d67e6f932d34c977ea50bf2903
François Gillet <gilletfrancois@gmail.com>
2013-07-20 21:26:07 +1100

    Replace preprocess HTML
    if not, it fails

--- custom/template.php
+++ custom/template.php
@@ -16,7 +16,22 @@ function custom_html_head_alter(&$head_elements) {
 
 
 function custom_preprocess_html(&$vars) {
-  
+  // Ensure that the $vars['rdf'] variable is an object.
+ if (!isset($vars['rdf']) || !is_object($vars['rdf'])) {
+    $vars['rdf'] = new StdClass();
+  }
+
+  if (module_exists('rdf')) {
+    $vars['doctype'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML+RDFa 1.1//EN">' . "\n";
+    $vars['rdf']->version = 'version="HTML+RDFa 1.1"';
+    $vars['rdf']->namespaces = $vars['rdf_namespaces'];
+    $vars['rdf']->profile = ' profile="' . $vars['grddl_profile'] . '"';
+  } else {
+    $vars['doctype'] = '<!DOCTYPE html>' . "\n";
+    $vars['rdf']->version = '';
+    $vars['rdf']->namespaces = '';
+    $vars['rdf']->profile = '';
+  }  
 }
 
 function custom_preprocess_page(&$vars) {
